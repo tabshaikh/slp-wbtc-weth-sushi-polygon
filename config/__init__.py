0c5b5fe43dd6ab8f68840acbc172283c052b8d08
Tabish Shaikh <tabish@archive.org>
2021-09-11 08:46:29 +0530

    config's added

--- config/__init__.py
+++ config/__init__.py
@@ -1,13 +1,13 @@
 ## Ideally, they have one file with the settings for the strat and deployment
 ## This file would allow them to configure so they can test, deploy and interact with the strategy
 
-BADGER_DEV_MULTISIG = "0xb65cef03b9b89f99517643226d76e286ee999e77"
+BADGER_DEV_MULTISIG = "0xc388750A661cC0B99784bAB2c55e1F38ff91643b"
 
-WANT = "0x6b175474e89094c44da98b954eedeac495271d0f"  ## Dai
-LP_COMPONENT = "0x028171bca77440897b824ca71d1c56cac55b68a3"  ## aDAI
-REWARD_TOKEN = "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"  ## AAVE Token
+WANT = "0xe62ec2e799305e0d367b0cc3ee2cda135bf89816"  ## WETH-SUSHI-SLP on polygon
+REWARD_TOKEN = "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"  ## Sushi Token
+# REWARD_TOKEN = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270"  ## wMatic Token
 
-PROTECTED_TOKENS = [WANT, LP_COMPONENT, REWARD_TOKEN]
+PROTECTED_TOKENS = [WANT, REWARD_TOKEN]
 ## Fees in Basis Points
 DEFAULT_GOV_PERFORMANCE_FEE = 1000
 DEFAULT_PERFORMANCE_FEE = 1000
